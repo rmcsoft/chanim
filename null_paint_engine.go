f04dd723d175cb8b83c2244854e37aaab64ef7c6
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-07-17 10:33:11 +0300

    Added NullPaintEngine

---
+++ null_paint_engine.go
@@ -0,0 +1,33 @@
+package chanim
+
+import (
+	"image"
+)
+
+type nullPaintEngine struct {
+}
+
+// NullPaintEngine returns null paint engine
+func NullPaintEngine() PaintEngine {
+	return nullPaintEngine{}
+}
+
+func (nullPaintEngine) Begin() error {
+	return nil
+}
+
+func (nullPaintEngine) Clear(rect image.Rectangle) error {
+	return nil
+}
+
+func (nullPaintEngine) DrawPixmap(top image.Point, pixmap *Pixmap) error {
+	return nil
+}
+
+func (nullPaintEngine) DrawPackedPixmap(top image.Point, packedPixmap *PackedPixmap) error {
+	return nil
+}
+
+func (nullPaintEngine) End() error {
+	return nil
+}
