52db289b1a51b5ca2031997fcdde6e51c0f6ff24
Alexey Alekseev <aleksey_alekseev@relex.ru>
2019-07-18 11:41:57 +0300

    Improved eqPixels

--- packed_pixmap.go
+++ packed_pixmap.go
@@ -240,17 +240,7 @@ func MMapPackedPixmap(fileName string) (*PackedPixmap, error) {
 }
 
 func eqPixels(a []byte, b []byte) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-
-	return true
+	return bytes.Equal(a, b)
 }
 
 // PackPixmap packs Pixmap
